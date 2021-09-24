7f87ccb3729e8ac4cd3551b531d47b009e1bb4b7
nathandrapeza <drapeza@ualberta.ca>
2021-09-23 23:31:47 -0600

    working on response construction

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,13 +27,154 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+# Citation:
+# https://stackoverflow.com/questions/2104080/how-can-i-check-file-size-in-python
+# User Danben, https://stackoverflow.com/users/217332/danben
+# Used his code for getting a file's size
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        #print ("Got a request of: %s\n" % self.data)
+        req_str = self.data.decode("utf-8")
+        #print(f"a: {a}, a[0]: {a[0]}")
+        #print(list(self.data.split(" ")))
+        start_line = self.grab_start_line(req_str)
+
+        request_data = self.process_location(start_line)
+        status_line = f"HTTP/1.1 {request_data[0]}"
+        print(f"STATUS LINE: {status_line}")
+
+        # All following request_data list variables will already have \n\r
+        status_code = request_data[0]
+        size = request_data[1]
+        content_type = request_data[2]
+        message = request_data[3]
+        
+        # Building the response:
+        response = status_line
+        response += "Connection: close\n\r"
+        response += f"Content-Length: 850\n\r"
+        #if not request_data[0.startswith("404"):
+
+        response += f"Content-Type: {content_type}"
+        response += "\n\r"
+        
+        if content_type.startswith("text"):
+            response += message
+
+        print(response)
+        
+        #self.request.sendall(bytearray("OK",'utf-8'))
+
+    
+    # grab_start_line() method will return an array of elements from the
+    # client's request startline
+    # request is formatted already (self.request.rec(2012.strip()))
+    # [method, target, http version]
+    def grab_start_line(self, request):
+        print(f"REQ: {request}")
+        print(request)
+        start_line = ""
+        i = 0
+        while i < len(request):
+            if request[i] != '\r':
+                start_line += request[i]
+                #print("%s", request[i])
+            else:
+                i = len(request)
+            i += 1
+
+        lst = list(start_line.split())
+        return lst
+
+    # process_location() method will process the location of a startline
+    # returns response code, content_type, size, and payload if applicable
+    # [response code, size, content type, payload]
+    def process_location(self, start_line):
+        root_dir = "./www"
+        size = "0\n\r"
+        content_type = None
+        status_code = None
+        payload = None
+        
+        if start_line[1] == "/":
+            try: # / indicates response should return /index.html from root dir
+                filename = f"{root_dir}/index.html"
+                payload = open(f"{root_dir}/index.html", "r")
+                status_code = "200 OK\n\r"
+                #content_type = "text/html; charset=UTF-8; charset=iso-8859-1"
+                content_type = "text/html; charset=UTF-8\n\r"
+                #size = os.path.getsize("{root_dir}/index.html")
+                #print(f"SIZEE::::: {size}")
+                payload = self.process_html_css(payload)
+            except:
+                status_code = "404 Not Found"
+        elif start_line[1].endswith("html") or start_line[1].endswith("css"):
+            try:
+                payload = open(f"{root_dir}/{start_line[1]}", "r")
+                status_code = "200 OK\n\r"
+                if start_line[1].endswith("html"):
+                    content_type = "text/html\n\r" #charset=UTF-8
+                    payload = self.process_html_css(payload)
+                else:
+                    content_type = "text/css\n\r"
+                    payload = self.process_html_css(payload)
+            except:
+                status_code = "404 Not Found\n\r"
+        else:
+            try:
+                payload = open(f"{root_dir}/{start_line[1]}", "r")
+                payload = self.process_html_css(payload)
+                status_code = "200 OK\n\r"
+            except:
+                try:
+                    # it might've given us a directory, namely /deep
+                    # therefore, try to find index.html inside of /.../dir/
+                    payload = open(f"{root_dir}/{start_line[1]}/index.html", "r")
+                    payload = self.process_html_css(payload)
+                    content_type = "text/html; charset=UTF-8\n\r"
+                except:
+                    status_code = "404 Not Found\n\r"
+        
+        #self.process_html(payload)
+        request_data = [status_code, size, content_type, payload]
+        #print(request_data)
+        return(request_data)
+
+    # process_html_css goes through an HTML or CSS file and returns one to be used in the
+    # server HTTP respnose
+    # input: payload is a python file object
+    def process_html_css(self, payload):
+        lines = []
+        for line in payload:
+            lines.append(line)
+        i = 0
+        while i < len(lines):
+            lines[i] = lines[i].replace("\t","")
+            i += 1
+        
+        i = 0
+        while i < len(lines):
+            j = 0
+            count = 0
+            spaces = False
+            print(lines[i])
+
+            while j < len(lines[i]):
+                if lines[i][j] == " ":
+                    count += 1
+                else:
+                    lines[i] = lines[i][count::]
+                    j = len(lines[i])
+                j += 1
+            i += 1
+
+        while "\n" in lines:
+            lines.remove("\n")
+        print(f"lines:{lines}")
+        message = "\r".join(lines)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
